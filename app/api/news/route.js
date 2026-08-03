5afa64f491e746077570d9a6801510759a656c69
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-03 22:28:14 +0530

    Refactor news API GET request and query handling

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -1,17 +1,11 @@
 export async function GET(request) {
-  const API_KEY = process.env.GNEWS_API_KEY; // 👈 idi important
-
+  const API_KEY = process.env.GNEWS_API_KEY;
   const { searchParams } = new URL(request.url);
   const type = searchParams.get('type') || 'top';
-
-  let query = '';
-  if(type === 'andhra') query = 'Andhra Pradesh';
-  if(type === 'sports') query = 'sports';
-  if(type === 'national') query = 'India';
-
-  const searchQuery = query? `&q=${query}` : '';
-
-  const url = `https://gnews.io/api/v4/top-headlines?lang=en&country=in&max=15${searchQuery}&apikey=${API_KEY}`;
+  let q = 'India';
+  if(type === 'andhra') q = 'Andhra Pradesh';
+  if(type === 'sports') q = 'Sports';
+  const url = `https://gnews.io/api/v4/search?q=${q}&lang=en&country=in&max=15&apikey=${API_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
   return Response.json(data);
