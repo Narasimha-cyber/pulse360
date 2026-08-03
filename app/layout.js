1806b6133b11d8d0d04495ea912a29c845fee3ed
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-03 11:16:29 +0530

    Update logo dimensions in layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
           <div className="container mx-auto px-4 py-3 flex items-center justify-between">
             
            {/* LOGO - INLINE STYLE */}
-         <div style={{ width: '160px', height: '32px' }}>
+         <div style={{ width: '300px', height: '48px' }}>
           <img
             src="/logo.png"
             alt="Pulse 360 NEWS"
