96d5202967717c8bdf4f3f1a5e1690f3a44604ad
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-03 11:55:15 +0530

    Change background size to 200% in layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
     width: '400px',
     height: '80px',
     backgroundImage: 'url(/logo.png)',
-    backgroundSize: 'contain',
+    backgroundSize: '200%',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'left center'
   }}></div>
