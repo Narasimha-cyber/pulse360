ceb5ae60fe7000f9486c05f48054964dc115322a
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-03 11:26:19 +0530

    Update logo size in layout header

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,14 @@ export default function RootLayout({ children }) {
        <header className="bg-white shadow-sm sticky top-0 z-50">
         <div className="container mx-auto px-4 py-3 flex items-center">
     
-         {/* LOGO - FORCE BIG SIZE */}
-        <div style={{ width: '350px', height: '60px', minWidth: '350px' }}>
-         <img
-          src="/logo.png"
-          alt="Pulse 360 NEWS"
-          style={{ width: '100%', height: '100%', objectFit: 'contain', display: 'block' }}
-          />
-         </div>
-
+       {/* LOGO - FORCE BIG SIZE */}
+     <div style={{ width: '400px', height: '70px', flexShrink: 0 }}>
+      <img
+       src="/logo.png"
+       alt="Pulse 360 NEWS"
+       style={{ width: '100%', height: '100%', objectFit: 'contain', display: 'block' }}
+      />
+   </div>
       </div>
     </header>
 
