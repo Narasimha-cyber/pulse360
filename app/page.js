4a011be6b151eab72c59bd83c04f6e289849adbd
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-03 18:46:23 +0530

    Reduce timeout duration from 39s to 19s

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
       const timer = setTimeout(() => {
         setShowIntro(false)
         onFinish()
-      }, 39000)
+      }, 19000)
 
       return () => clearTimeout(timer)
     }, [])
