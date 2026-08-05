97bd94582d64b67559df1f3019a9e9469ed2c4b8
Narasimha-cyber <narasimharaokilli@gmail.com>
2026-08-05 16:23:30 +0530

    Include Google Ads script in RootLayout
    Added Google Ads script to the layout.

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3333852580300958"
+     crossorigin="anonymous"></script>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
